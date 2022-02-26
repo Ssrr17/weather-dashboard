ae38c3a5406f566c6181d63a49989a9af38f2ee1
Salieu Sarr <95061054+Ssrr17@users.noreply.github.com>
2022-02-26 15:42:11 -0500

    page stucture added

---
+++ assets/JS/script.js
@@ -0,0 +1,7 @@
+//Get user input 
+//use user input to request target data  from api
+//display city name, date, icon to represent wheather condition, tempreture, humidity, windspeed and the UV index
+//add color codered respose for favorable, moderate or severe when UV index is viewed
+//add future weather for next 5days 
+//save city results in search history
+//reload city results from search history
